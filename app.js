afd6110edfb61d82018bf1cad365248c99e60bf8
dankturtle <abrowncownow@gmail.com>
2022-12-10 21:44:26 -0800

    console.table formatted correctly

--- app.js
+++ app.js
@@ -2,30 +2,35 @@ const db = require("./db/connection.js");
 const cTable = require("console.table");
 const DBFuncs = require('./db/DBFuncs.js');
 const inquirer = require("inquirer");
-const { addDepartment, getAllEmployees } = require("./db/DBFuncs.js");
+const sleep = (ms = 3000) => new Promise ((r)=> setTimeout(r,ms))
+
 
 async function menu(){
     const answers = await inquirer.prompt([{
         name: 'choice',
-        type: 'choice',
+        type: 'list',
         choices: ['View All Departments', 'Add Department', 'View All Roles', 'Add Role','View All Employees', 'Add Employee'],
         message: 'What would you like to do?'
-    }])
+    }]);
     switch (answers.choice){
         case 'View All Departments':
-            DBFuncs.getAllDepartments();
+            DBFuncs.getAllDepartments()
+            sleep();
+            menu();
             break;
         case 'Add Department':
             addDepartment();
             break;
         case 'View All Roles':
             DBFuncs.getAllRoles();
+            menu();
             break;
         case 'Add Role':
             addRole();
             break;
         case 'View All Employees':
             DBFuncs.getAllEmployees();
+            menu();
             break;
         case 'Add Employee':
             addEmployee();
@@ -34,11 +39,6 @@ async function menu(){
 }
 
 async function addEmployee(){
-    //get and format role_id name
-    //         choices: [{
-    //          name: 'departmentname' 
-    //          id: 'department_id'
-    //          }]
     const answers = await inquirer.prompt([{
         name: 'first_name',
         type: 'input',
@@ -59,10 +59,11 @@ async function addEmployee(){
         name: 'manager_id',
         type: 'input',
         message: "Input new employee's manager id........"
-    }])
+    }]);
 
     DBFuncs.addEmployee(answers);
     DBFuncs.getAllEmployees();
+    menu();
 }
 
 async function addRole(){
@@ -81,10 +82,11 @@ async function addRole(){
         type: 'number',
         message: "Input new role's department id.......",
 
-    }])
+    }]);
 
     DBFuncs.addRole(answers);
     DBFuncs.getAllRoles();
+    menu();
 }
 
 async function addDepartment(){
@@ -92,18 +94,11 @@ async function addDepartment(){
         name: 'department',
         type: 'input',
         message: 'What is the name of the department?'
-    }])
+    }]);
 
     DBFuncs.addDepartment(answers);
     DBFuncs.getAllDepartments();
+    menu();
 }
+menu();
 
-
-//initialize application
-//ask questions
-//functions that will run prompts
-
-// function viewRoles() {
-//     ClassDB.findAllRoles()
-//     .then(([rows]))
-// }
--- db/DBFuncs.js
+++ db/DBFuncs.js
@@ -1,5 +1,6 @@
 //Import database connection
 const db = require("./connection.js");
+const cTable = require("console.table");
 
 //class with query functions
 class DBFuncs {
@@ -10,24 +11,28 @@ class DBFuncs {
     getAllDepartments() {
         this.connection.query("select * from departments", (err, res) => {
           if (err) throw err;
+          console.log("\n")
           return console.table(res);
         });
     }
     getAllRoles(){
         this.connection.query("SELECT * FROM roles", (err, res) => {
             if (err) throw err;
+            console.log("\n");
             return console.table(res);
           })
     }
     getAllEmployees(){
         this.connection.query("select * from employees", (err, res) => {
             if (err) throw err;
+            console.log("\n");
             return console.table(res);
           });
     }
     addDepartment(department){
         this.connection.query("INSERT INTO departments set ?", department, (err, res) => {
              if (err) throw err;
+             console.log("\n");
             return console.table(res);
         })
     }
@@ -35,12 +40,14 @@ class DBFuncs {
     addRole(role){
         this.connection.query('INSERT INTO roles set ?', role, (err, res) => {
             if (err) throw err;
+            console.log("\n");
            return console.table(res);} )
     }
     //AddAEmployee
     addEmployee(employee){
         this.connection.query('INSERT INTO employees set ?', employee, (err, res) => {
             if (err) throw err;
+            console.log("\n");
            return console.table(res);} )
     }
     //UpdateAnEmployee
